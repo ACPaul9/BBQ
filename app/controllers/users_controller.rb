4e892411c4aa1727fc27d74685dfbf7909dcceab
ACPaul9 <alex.zephaniah244@gmail.comgit config --global user.email alex.zephaniah244@gmail.com>
2022-06-09 21:22:19 +0300

    Change design registration page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_current_user, except: [:show]
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -11,14 +13,14 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       redirect_to @user, notice: 'User was successfully updated.'
     else
-      render :edit
+      render :edit,  notice: 'Something wrong'
     end
   end
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
+  def set_current_user
+    @user = current_user
   end
 
   # Пропишем, что разрешено передавать в params
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-warning',
+      alert: 'alert-danger',
+      notice: 'alert-success'
+    }.stringify_keys[flash_type] || flash_type
+  end
+
+  def bootstrap_flash
+    flash.each do |type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(type)} alert-dismissible fade show mt-4", role: 'alert') do
+        concat content_tag(:button, '',type: 'button', class: 'btn-close', data: { bs_dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
+
+  def user_avatar(user)
+    asset_path('user.png')
+  end
 end
