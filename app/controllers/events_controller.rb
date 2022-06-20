8ecc6637b24c02e0605e791f3e6cf66f5f088578
ACPaul9 <alex.zephaniah244@gmail.comgit config --global user.email alex.zephaniah244@gmail.com>
2022-06-20 21:05:33 +0300

    Add password and yandex map to event page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   before_action :set_event, only: [:show]
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
+  before_action :password_guard!, only: [:show]
 
   def index
     @events = Event.all
@@ -56,6 +57,26 @@ class EventsController < ApplicationController
 
   # редактируем параметры события
   def event_params
-    params.require(:event).permit(:title, :address, :datetime, :description, :photo)
+    params.require(:event).permit(:title, :address, :datetime, :description, :photo, :pincode)
   end
+
+  def password_guard!
+    # Если у события нет пин-кода, то охранять нечего
+    return true if @event.pincode.blank?
+    # Пин-код не нужен автору события
+    return true if signed_in? && current_user == @event.user
+
+    if params[:pincode].present? && @event.pincode_valid?(params[:pincode])
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
+    end
+
+    pincode = cookies.permanent["events_#{@event.id}_pincode"]
+    unless @event.pincode_valid?(pincode)
+      if params[:pincode].present?
+        flash.now[:alert] = I18n.t('controllers.events.wrong_pincode')
+      end
+      render 'password_form'
+    end
+  end
+
 end
