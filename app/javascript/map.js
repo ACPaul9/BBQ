2a9b20351eb2c443cf6534b76dfd57b511c284af
ACPaul9 <alex.zephaniah244@gmail.comgit config --global user.email alex.zephaniah244@gmail.com>
2022-06-21 16:23:17 +0300

    bug fix with map display

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,31 +1,34 @@
-ymaps.ready(init);
+document.addEventListener('turbo:load', () => {
+  ymaps.ready(init);
+  let myMap;
 
-function init() {
+  function init() {
     address = document.getElementById('map').getAttribute('data-address');
 
-    myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 10
+    myMap = new ymaps.Map('map', {
+      center: [55.76, 37.64],
+      zoom: 10
     });
 
     myGeocoder = ymaps.geocode(address);
 
-    myGeocoder.then(
+      myGeocoder.then(
         function (res) {
-            coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+          coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
             myMap.geoObjects.add(
-                new ymaps.Placemark(
-                    coordinates,
-                    { iconContent: address },
-                    { preset: 'islands#blueStretchyIcon' }
-                )
+              new ymaps.Placemark(
+                coordinates,
+                { iconContent: address },
+                { preset: 'islands#blueStretchyIcon' }
+              )
             );
 
             myMap.setCenter(coordinates);
             myMap.setZoom(15);
-        }, function (err) {
-            alert('Ошибка при определении местоположения');
-        }
-    );
-}
+          }, function (err) {
+               alert('Ошибка при определении местоположения');
+             }
+      );
+  }
+})
